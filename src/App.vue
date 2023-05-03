c3a4cc78a0420901af0e7fdc3a542e87664a5075
NelsonRyan-06 <nelsonarabit00@gmail.com>
2023-05-03 11:24:23 +0800

    Vue tutorial start

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <navbar
+    :pages="pages"
+    :page-index="pageIndex"
+    :click-navbar="(index) => (pageIndex = index)"
+  ></navbar>
+  <page-viewer :page="pages[pageIndex]"></page-viewer>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import PageViewer from "./components/PageViewer.vue";
+
+export default {
+  components: {
+    Navbar,
+    PageViewer,
+  },
+  data() {
+    return {
+      pageIndex: 0,
+      pages: [
+        {
+          link: {
+            navTitle: "Home",
+            url: "index.html",
+          },
+          pageTitle: "Hello, Home",
+          content: "Welcome to the world of Vue and Laravel",
+        },
+        {
+          link: {
+            navTitle: "About",
+            url: "about.html",
+          },
+          pageTitle: "Hello, About",
+          content: "Welcome to the world of Vue and Laravel",
+        },
+        {
+          link: {
+            navTitle: "Contact",
+            url: "contact.html",
+          },
+          pageTitle: "Hello, Contact",
+          content: "Welcome to the world of Vue and Laravels",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav
+    :class="['navbar', 'navbar-expand-lg', `navbar-${theme}`, `bg-${theme}`]"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul id="links" class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li v-for="(page, index) in pages" class="nav-item" :key="index">
+            <a
+              class="nav-link"
+              :class="{ active: pageIndex == index }"
+              aria-current="page"
+              :href="page.link.url"
+              :title="`The link is goes to ${page.link.navTitle} page`"
+              @click.prevent="clickNavbar(index)"
+              >{{ page.link.navTitle }}</a
+            >
+          </li>
+        </ul>
+        <div class="d-flex">
+          <button class="btn btn-primary" @click.prevent="changeThemeColor()">
+            Toggle Navbar
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["pages", "pageIndex", "clickNavbar"],
+
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+  methods: {
+    changeThemeColor() {
+      let theme = "light";
+
+      if (this.theme == "light") {
+        theme = "dark";
+      }
+
+      this.theme = theme;
+    },
+  },
+};
+</script>
